24572e4688fbee3f603ad8c15930737c74e75d67
haky1313 <haky1313@gmail.com>
2021-10-10 23:50:20 +0530

    for jenkins

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -27,6 +27,7 @@ public class base {
 		prop=new Properties();
 		//FileInputStream fis=new FileInputStream("C:\\Users\\haky1\\eclipse-workspace\\E2EProject\\src\\main\\java\\resources\\data.properties");
 		FileInputStream fis=new FileInputStream(projectpath+"\\src\\main\\java\\resources\\data.properties");
+		//FileInputStream fis=new FileInputStream(".\\data.properties");
 		
 		prop.load(fis);
 		String browserName=prop.getProperty("browser");
